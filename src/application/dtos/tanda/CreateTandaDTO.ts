d42163b1b155ede7a9269aa8acf44a5d281bd206
DidiAndroidP <didier.mendoza.uni@gmail.com>
2026-02-08 14:21:19 -0600

    add

--- src/application/dtos/tanda/CreateTandaDTO.ts
+++ src/application/dtos/tanda/CreateTandaDTO.ts
@@ -1,11 +1,8 @@
-import { PaymentFrequency } from "../../../domain/entities/Tanda"
-
 export interface CreateTandaDTO {
   name: string
   contributionAmount: number
-  paymentFrequency: PaymentFrequency
+  paymentFrequency: string
   totalMembers: number
   delayToleranceDays: number
   penaltyPerDay: number
-  creatorId: number
 }
